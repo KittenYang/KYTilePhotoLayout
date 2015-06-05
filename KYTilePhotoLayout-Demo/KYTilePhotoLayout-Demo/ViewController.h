68d86995596140fee4a3d36b91e805d38a127451
KittenYang <kittenyang@icloud.com>
2015-06-05 14:43:55 +0800

    initial commit

---
+++ KYTilePhotoLayout-Demo/KYTilePhotoLayout-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KYTilePhotoLayout-Demo
+//
+//  Created by Kitten Yang on 6/5/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KYTilePhotoLayout-Demo/KYTilePhotoLayout-Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KYTilePhotoLayout-Demo
+//
+//  Created by Kitten Yang on 6/5/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
